8ab355fcb4070a1533429179ab9746e2fdc97248
Camille-Antoine Fontaine <Cfontainef1@gmail.com>
2017-12-05 15:40:42 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export default (callback) => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://<dbuser>:<dbpassword>@ds115131.mlab.com:15131/heroku_qtsch1bk');
+    mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://<dbuser>:<dbpassword>@ds115131.mlab.com:15131/heroku_qtsch1bk');
     callback();
 };
